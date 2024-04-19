2e2c7099bf48fd3c35c4958e001054c695b22aa7
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-04-19 20:56:05 -0300

    Create inicio_FOR.py

---
+++ COMPUTATIONAL THINKING WITH PYTHON/Aulas_/inicio_FOR.py
@@ -0,0 +1,31 @@
+'''for char in 'Hellen':
+    print(char)
+
+for i in range(1,10,2): de 1 até 10 de 2 em 2 vai somando
+     print(i)
+
+#start,end,step
+for i in range(10,0,-2): de 10 até 0 de -2 em -2 vai ssubtraindo
+     print(i)
+
+altera i dentro do for no while nao da certo, aqui o for passa por todos elementos de um conjunto pre determinado
+ele semper vai para o proximo passo, ele sempre busca no conjunto inicial o que tem que fazer
+
+for i in range(10):
+    print(i,end = ' ')
+    i = 1
+    print(i)
+
+# todos os pares de 0 a 10
+
+for i in range(10):
+     if i%2 == 0:
+         print(f"{i} é par")
+else:
+    print(f"{i} é ímpar")
+
+#comprimento do nome analogo do len
+i=0
+for char in 'hel':
+    i+=1
+print(i)'''
