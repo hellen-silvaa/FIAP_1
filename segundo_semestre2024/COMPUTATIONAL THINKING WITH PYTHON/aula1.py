eec54152ee49e8484693fdf2e338f0204c827eca
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-08-09 20:59:38 -0300

    Create aula1.py

---
+++ segundo_semestre2024/COMPUTATIONAL THINKING WITH PYTHON/aula1.py
@@ -0,0 +1,23 @@
+'''number = int(input ('Digite um número: '))
+print(number/2)
+
+print(hex(15))
+print(bin(15))
+print(int(0b1000))
+print(int(0xa))
+
+numero = 7
+print(numero // 2)
+
+numero = 7
+print(numero % 2)
+
+raiz quadrada eleva a meio e raiz cubica eleva a um terço  mn
+numero = 49
+print(numero % 0.5)'''
+
+
+nome_do_aluno = ''' Algum nome feio 
+
+                       tipo enzo
+'''
